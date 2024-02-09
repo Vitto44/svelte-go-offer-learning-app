28610ccb6a3229858cf40fb36deeb154bd6a4edd
Vito <vi.h.seo@gmail.com>
2024-02-09 23:11:29 +0100

    add + style hero section

---
+++ src/lib/Hero.svelte
@@ -0,0 +1,27 @@
+<script>
+	import heroImage from "$lib/images/hero-image.png";
+</script>
+
+<header>
+	<div
+		class="bg-gradient-to-r from-primary to-cyan-600 text-stone-100 max-w-5xl mx-auto rounded-xl p-8 grid grid-cols-3 relative mt-28 shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]"
+	>
+		<div class=" flex gap-4 md:gap-8 flex-col col-span-3 sm:col-span-2">
+			<h1 class="font-semibold text-2xl md:text-5xl">
+				Shortcut to <span class="text-secondary-200 font-bold">Free Bets</span>
+				<br class="hidden lg:inline-block" />
+				and
+				<span class="text-secondary-200 font-bold">Wins</span>!
+			</h1>
+			<h2 class="text-sm md:text-base lg:text-lg sm:mr-12 lg:mr-0">
+				Get the latest free bets and bonuses from the best bookmakers and casinos. We have the best
+				free bets and bonuses for you to claim and win real money.
+			</h2>
+		</div>
+		<img
+			src={heroImage}
+			alt="hero"
+			class="absolute max-w-36 sm:max-w-64 md:max-w-80 right-0 -top-24 sm:-top-16"
+		/>
+	</div>
+</header>
