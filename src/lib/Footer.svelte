d483335ca983852f23f8323e59537c79ad327a1b
Vito <vi.h.seo@gmail.com>
2024-02-10 19:20:31 +0100

    add footer to complkete layout

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+	import { goto } from "$app/navigation";
+
+	$: currentPage = $page.route.id;
+
+	const navigate = (/** @type {string | URL} */ url: string) => {
+		if (url === currentPage) return;
+		goto(url);
+	};
+</script>
+
+<footer class="max-w-5xl mx-auto gap-8 my-8">
+	<div
+		class="flex flex-wrap justify-center gap-4 bg-gray-100 px-8 py-4 mx-4 rounded-2xl shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]"
+	>
+		<p class="text-text-primary bg-gray-800 px-4 py-2 rounded-md">© Certain Wager</p>
+		<ul class="flex gap-3 py-2">
+			<a href="/reviews" on:click|preventDefault={() => navigate("/reviews")}>Reviews</a>
+
+			<a href="/" on:click|preventDefault={() => navigate("/")}>Home</a>
+
+			<a href="/blog" on:click|preventDefault={() => navigate("/blog")}>Blog</a>
+		</ul>
+	</div>
+</footer>
