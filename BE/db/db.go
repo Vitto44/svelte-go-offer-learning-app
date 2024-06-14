2f640eb501e1947faaf40102988c54dc51cee668
Vito <vi.h.seo@gmail.com>
2024-06-14 23:59:35 +0200

    shambles spagheti on BE

---
+++ BE/db/db.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+var db *sql.DB
+
+func initDB() {
+	var err error
+	connStr := "user=postgres dbname=certainwager sslmode=disable password=0000 host=172.18.96.1"
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully connected to database!")
+
+	createTables()
+}
+
+func createTables() {
+	tableCreationQueries := []string{
+		`CREATE TABLE IF NOT EXISTS offers (
+			id SERIAL PRIMARY KEY,
+			title TEXT NOT NULL,
+			description TEXT NOT NULL
+		)`,
+		`CREATE TABLE IF NOT EXISTS reviews (
+			id SERIAL PRIMARY KEY,
+			title TEXT NOT NULL,
+			content TEXT NOT NULL,
+			rating INTEGER NOT NULL
+		)`,
+		`CREATE TABLE IF NOT EXISTS blogs (
+			id SERIAL PRIMARY KEY,
+			title TEXT NOT NULL,
+			content TEXT NOT NULL
+		)`,
+	}
+
+	for _, query := range tableCreationQueries {
+		_, err := db.Exec(query)
+		if err != nil {
+			log.Fatalf("Error creating table: %v", err)
+		}
+	}
+
+	fmt.Println("Tables created or already exist.")
+}
--- BE/go.mod
+++ BE/go.mod
@@ -1,3 +1,7 @@
 module certainwager-be
 
 go 1.22.4
+
+require github.com/lib/pq v1.10.9
+
+require github.com/gorilla/mux v1.8.1
